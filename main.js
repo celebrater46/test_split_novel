4ae53a72f9a4925c46e93fe86e1f6fed29f4d26a
celebrater <tkgm45nbchk@gmail.com>
2022-02-08 17:48:21 +0900

    test to separate lines each 40 chars succeeded

--- main.js
+++ main.js
@@ -2,6 +2,24 @@
 
 const novel = document.getElementById("novel");
 
+const splitNovel40 = (novel) => {
+    const separated = novel.split("\n");
+    let str = "";
+    separated.map((line) => {
+        let temp = line;
+        while(temp.length > 40){
+            str += "<p>" + temp.substring(0, 40) + "</p>";
+            temp = temp.substring(41);
+        }
+        str += "<p>" + temp + "</p>";
+        // if(line.length > 40){
+        //     str += "<p>" + line + "</p>";
+        // }
+        // str += "<p>" + (line === "" ? "　" : line) + "</p>";
+    });
+    return str;
+}
+
 const splitNovel = (novel) => {
     const separated = novel.split("\n");
     let str = "";
@@ -11,4 +29,5 @@ const splitNovel = (novel) => {
     return str;
 }
 
-novel.innerHTML = splitNovel(sample);
+// novel.innerHTML = splitNovel(sample);
+novel.innerHTML = splitNovel40(sample);
