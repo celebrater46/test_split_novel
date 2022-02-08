e16f2d74e677854c326bf28ca1bae82dd5283730
celebrater <tkgm45nbchk@gmail.com>
2022-02-08 17:36:10 +0900

    cleaned up code

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 "use strict";
 
-// console.log(sample);
-
 const novel = document.getElementById("novel");
 
 const splitNovel = (novel) => {
     const separated = novel.split("\n");
     let str = "";
     separated.map((line) => {
-        // return "<p>" + line + "</p>";
         str += "<p>" + (line === "" ? "　" : line) + "</p>";
     });
     return str;
 }
 
-const convertToHtml = (array) => {
-
-}
-
-// novel.innerText = sample;
-// console.log(splitNovel(sample));
-// novel.innerHTML = splitNovel(sample); // Unexpected ","
 novel.innerHTML = splitNovel(sample);
