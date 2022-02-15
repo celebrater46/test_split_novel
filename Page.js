8dc62fb883be1be38bb890f5ddf2896d071fe1c5
celebrater <tkgm45nbchk@gmail.com>
2022-02-15 21:18:21 +0900

    test to add lines within the height of page succeeded

--- Page.js
+++ Page.js
@@ -14,14 +14,33 @@ class Page {
     createPage(){
         let container = document.getElementById("containter");
         let page = document.createElement("div");
+        container.appendChild(page);
+        const pHeight = document.getElementById("scale_p").clientHeight;
         page.id = "p-" + this.id;
         page.classList.add("page");
+        let currentHeight = 0;
         this.lines.map((line) => {
-            let p = document.createElement("p");
-            p.innerText = line;
-            page.appendChild(p);
+            if(page.clientHeight < maxHeight){
+                console.log(page.clientHeight);
+                let p = document.createElement("p");
+                p.innerText = line;
+                page.appendChild(p);
+                // console.log(pHeight);
+                // console.log(page.clientHeight);
+                currentHeight += pHeight;
+                // currentHeight = page.clientHeight;
+            }
+            // while(currentHeight < maxHeight){
+            //     let p = document.createElement("p");
+            //     p.innerText = line;
+            //     page.appendChild(p);
+            //     currentHeight = page.clientHeight;
+            // }
         });
-        container.appendChild(page);
+        // container.appendChild(page);
+        // console.log(page.clientHeight);
+        console.log(maxHeight);
+        // console.log(pHeight);
     }
 
     test(){
