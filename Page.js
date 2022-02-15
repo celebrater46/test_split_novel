480675a6bdf4f0a0e0525928451d9fdb40978226
celebrater <tkgm45nbchk@gmail.com>
2022-02-15 21:33:46 +0900

    got the final line successfully

--- Page.js
+++ Page.js
@@ -19,27 +19,48 @@ class Page {
         page.id = "p-" + this.id;
         page.classList.add("page");
         let currentHeight = 0;
-        this.lines.map((line) => {
+        let finalLine = 0;
+        for(let i = 0; i < this.lines.length; i++){
             if(page.clientHeight < maxHeight){
                 console.log(page.clientHeight);
                 let p = document.createElement("p");
-                p.innerText = line;
+                p.innerText = this.lines[i];
                 page.appendChild(p);
                 // console.log(pHeight);
                 // console.log(page.clientHeight);
                 currentHeight += pHeight;
                 // currentHeight = page.clientHeight;
+            } else {
+                if(finalLine === 0){
+                    finalLine = i - 1;
+                }
             }
-            // while(currentHeight < maxHeight){
-            //     let p = document.createElement("p");
-            //     p.innerText = line;
-            //     page.appendChild(p);
-            //     currentHeight = page.clientHeight;
-            // }
-        });
-        // container.appendChild(page);
-        // console.log(page.clientHeight);
+        }
+        console.log(this.lines[finalLine]);
+        console.log(page.clientHeight);
         console.log(maxHeight);
+
+
+        // let i = 0;
+        // this.lines.map((line) => {
+        //     // if(page.clientHeight < maxHeight){
+        //     //     console.log(page.clientHeight);
+        //     //     let p = document.createElement("p");
+        //     //     p.innerText = line;
+        //     //     page.appendChild(p);
+        //     //     // console.log(pHeight);
+        //     //     // console.log(page.clientHeight);
+        //     //     currentHeight += pHeight;
+        //     //     // currentHeight = page.clientHeight;
+        //     // }
+        //     // while(currentHeight < maxHeight){
+        //     //     let p = document.createElement("p");
+        //     //     p.innerText = line;
+        //     //     page.appendChild(p);
+        //     //     currentHeight = page.clientHeight;
+        //     // }
+        // });
+        // container.appendChild(page);
         // console.log(pHeight);
     }
 
